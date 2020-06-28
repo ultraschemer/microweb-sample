3ba884c05d0ad390d82b67f3750e0a694058c4d9
Paulo Silva Filho <paulosfilho@gmail.com>
2020-06-28 10:54:51 -0300

    Comments in the sample.

--- src/main/java/microweb/sample/controller/DefaultHomePageController.java
+++ src/main/java/microweb/sample/controller/DefaultHomePageController.java
@@ -7,8 +7,10 @@ import io.vertx.ext.web.RoutingContext;
 import microweb.sample.view.FtlHelper;
 
 public class DefaultHomePageController extends SimpleController {
+    // C.1: Create a static instance of the default home page template variable, to store and cache it:
     private static Template homePageTemplate = null;
 
+    // C.2: Initialize the template defined above, suitably:
     static {
         try {
             homePageTemplate = FtlHelper.getConfiguration().getTemplate("homePage.ftl");
@@ -19,15 +21,17 @@ public class DefaultHomePageController extends SimpleController {
         }
     }
 
+    // C.3: Define the default controller constructor:
     public DefaultHomePageController() {
         super(500, "a37c914b-f737-4a73-a226-7bd86baac8c3");
     }
 
     @Override
     public void executeEvaluation(RoutingContext routingContext, HttpServerResponse httpServerResponse) throws Throwable {
+        // C.4: In the controller evaluation routine, render the template:
         routingContext
-                .response()
-                .putHeader("Content-type", "text/html")
-                .end(FtlHelper.processToString(homePageTemplate, null));
+                 .response()
+                 .putHeader("Content-type", "text/html")
+                 .end(FtlHelper.processToString(homePageTemplate, null));
     }
 }
